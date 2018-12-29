d27f1d1e50acb99c22b89970b7d8f3a889fc3a20
Patrick <ppevans11@gmail.com>
2018-12-29 17:30:07 +1300

    added favicon and version 0.1.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render(){
-    return((
+    return(
       <div>
         <h1>App component</h1>
       </div>
